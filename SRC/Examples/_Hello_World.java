cc95fc71b783ad3a4898a6cd304d23dde46401f5
Jake Gadaleta <jake.gads@gmail.com>
2020-01-13 11:09:40 -0500

    added some folders to ignore

--- SRC/Examples/_Hello_World.java
+++
@@ -1,9 +0,0 @@
-//package SRC.Examples;
-public class _Hello_World {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
-
-}
